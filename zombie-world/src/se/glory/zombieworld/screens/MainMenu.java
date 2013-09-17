3577adb6b2bceeca830641ed74107c17f2b03a5c
Maestro <adam.jilsen@gmail.com>
2013-09-17 15:04:38 +0200

    Added splash screen, main menu and tween

---
+++ zombie-world/src/se/glory/zombieworld/screens/MainMenu.java
@@ -0,0 +1,122 @@
+package se.glory.zombieworld.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class MainMenu implements Screen {
+	private Stage stage;
+	private TextureAtlas atlas;
+	private Skin skin;
+	private Table table;
+	private TextButton buttonPlay, buttonExit;
+	private BitmapFont font;
+	private Label heading;
+    
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(255, 255, 255, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void show() {
+		stage = new Stage();
+		
+		Gdx.input.setInputProcessor(stage);
+		
+		atlas = new TextureAtlas("ui/button.pack");
+		skin = new Skin(atlas);
+		
+		//Create layouttable
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		//Init font
+		font = new BitmapFont(Gdx.files.internal("font/font.fnt"), false);
+		
+		//Create buttonstyle
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.getDrawable("button_up");
+		textButtonStyle.down = skin.getDrawable("button_down");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = font;
+		
+		LabelStyle headingStyle = new LabelStyle(font, Color.BLACK);
+		
+		heading = new Label("Main menu", headingStyle);
+		heading.setFontScale(2);
+		
+		//Create buttons
+		buttonExit = new TextButton("EXIT", textButtonStyle);
+		buttonPlay = new TextButton("PLAY", textButtonStyle);
+		
+		//Adding listeners to buttons
+		buttonExit.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Gdx.app.exit();
+			}
+		});
+		buttonPlay.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu());//Här sätter du vart den ska länka
+			}
+		});
+		
+		//Add padding to buttontext
+		buttonExit.pad(3);
+		buttonPlay.pad(3);
+		
+		table.add(heading);
+		table.row();
+		table.add(buttonPlay);
+		table.row();
+		table.add(buttonExit);
+		stage.addActor(table);
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+}
