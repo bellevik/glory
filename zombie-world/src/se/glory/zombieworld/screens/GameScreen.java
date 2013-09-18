cd2426742a90e8bb2f5f4a9394fe190046ca179f
Philip Ekman <ekman1991@gmail.com>
2013-09-18 11:07:56 +0200

    Player movement and moving camera

--- zombie-world/src/se/glory/zombieworld/screens/GameScreen.java
+++ zombie-world/src/se/glory/zombieworld/screens/GameScreen.java
@@ -1,36 +1,60 @@
 package se.glory.zombieworld.screens;
 
+import se.glory.entities.Player;
+import se.glory.utilities.Constants;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class GameScreen implements Screen {
 	
 	private OrthographicCamera camera;
+	private Box2DDebugRenderer debugRenderer;
 	private World world;
+	private Player player;
+	private SpriteBatch batch;
+	private Texture bkg;
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		camera.update();		
+		
+		camera.position.set(player.getPlayerBody().getPosition().x * Constants.BOX_TO_WORLD, player.getPlayerBody().getPosition().y * Constants.BOX_TO_WORLD, 0);
+		camera.update();
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bkg, 0, 0);
+		batch.end();
+		player.draw(batch);
 		
 		world.step(1/60f, 6, 2);
+		debugRenderer.render(world, camera.combined);
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		
+		camera.viewportWidth = width;
+        camera.viewportHeight = height;
 	}
 
 	@Override
 	public void show() {
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false);
 		world = new World(new Vector2(0, 0), true);
+		player = new Player (world, 100, 100, 32, 32);
+		debugRenderer = new Box2DDebugRenderer();
+		batch = new SpriteBatch();
+		Gdx.input.setInputProcessor(player);
+		bkg = new Texture(Gdx.files.internal("img/bkg.png"));
 	}
 
 	@Override
