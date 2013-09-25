3b4bd59d3282687061ceb5e7a12ad8847e6e53e7
VitoInfinito <tomas@hasselquist.se>
2013-09-25 14:29:02 +0200

    Added JUnit jars and a test case to begin with

--- zombie-world/src/se/glory/test/MainTest.java
+++
@@ -1,7 +0,0 @@
-package se.glory.test;
-
-import se.glory.*;
-
-public class MainTest {
-
-}
---
+++ zombie-world/src/se/glory/test/TestCases.java
@@ -0,0 +1,45 @@
+package se.glory.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+import se.glory.entities.Creature;
+import se.glory.entities.Human;
+import se.glory.entities.Zombie;
+
+public class TestCases {
+
+	
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testReactions() {
+	//	assertTrue("truetrue", new Vector2(20,0).x==20);
+	//	World testWorld = new World(new Vector2(0, 0), true);
+	//	ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+	//	ArrayList<Creature> humans = new ArrayList<Creature>();
+//		zombies = new ArrayList<Zombie>();
+//		humans = new ArrayList<Creature>();
+		
+	//	Human testHuman = new Human(testWorld, 150, 150);
+	//	humans.add(testHuman);
+	//	zombies.add(new Zombie(testWorld, 200,150));
+		
+	//	assertsEquals("Human starts at correct x position", 150, 150);
+	//	assertsEquals(150, 150);
+	//	assertTrue("truetrue", testHuman.getPosition().x==150);
+//		assertsEquals("Human starts at correct y position", expected, actual)
+		
+	//	testHuman.autoUpdateMovement(zombies);
+	}
+}
