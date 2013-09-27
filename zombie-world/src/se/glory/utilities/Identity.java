e0ab00cd5cfeb8d8a42d6ecbb4806b4657e31a53
Philip Ekman <ekman1991@gmail.com>
2013-09-27 10:31:07 +0200

    Identity class that will be set as the UserData of every Box2d body.
    This will ease the drawing procedure for one. Later on it will heal us
    to delete killed bodies.

---
+++ zombie-world/src/se/glory/utilities/Identity.java
@@ -0,0 +1,45 @@
+package se.glory.utilities;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/*
+ * This class will be the UserData object of every Box2d Body that will persist in 
+ * our Box2d World.
+ */
+public class Identity {
+	private Texture texture;
+	private boolean isDead = false;
+	private String type;
+	private float width, height;
+	
+	public Texture getTexture() {
+		return texture;
+	}
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+	public boolean isDead() {
+		return isDead;
+	}
+	public void setDead(boolean isDead) {
+		this.isDead = isDead;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public float getWidth() {
+		return width;
+	}
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+}
