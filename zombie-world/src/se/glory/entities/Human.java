8f6a0d2b26b864f20026a2fb66416abbffe97457
Philip Ekman <ekman1991@gmail.com>
2013-09-30 20:11:11 +0200

    Removed unused imports

--- zombie-world/src/se/glory/entities/Human.java
+++ zombie-world/src/se/glory/entities/Human.java
@@ -1,21 +1,8 @@
 package se.glory.entities;
 
 import se.glory.utilities.Constants;
-import se.glory.utilities.Identity;
-import se.glory.utilities.WorldHandler;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.World;
 
 public class Human extends MoveableBody {
-	
 	
 	public Human(int x, int y) {
 		super(x, y, 15, 15, null, Constants.MoveableBodyShape.CIRCLE , Constants.MoveableBodyType.HUMAN);
