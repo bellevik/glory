c8057a3931ec82bfa75eda3dca7e86a31c388871
Philip Ekman <ekman1991@gmail.com>
2013-09-30 19:32:48 +0200

    Moved the CollisionDetection code to its own class.
    The GameScreen class is getting very long and hard to read. We are
    moving code snippets out from the class to more descriptive classes

---
+++ zombie-world/src/se/glory/utilities/CollisionDetection.java
@@ -0,0 +1,37 @@
+package se.glory.utilities;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+/*
+ * This class will handle all of the Collisions in our Box2D world.
+ */
+public class CollisionDetection implements ContactListener {
+	
+	@Override
+	public void beginContact(Contact contact) {
+		Body a = contact.getFixtureA().getBody();
+		Body b = contact.getFixtureB().getBody();
+		String typeA = ((Identity)(a.getUserData())).getType();
+		String typeB = ((Identity)(b.getUserData())).getType();
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+		
+	}
+
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold) {
+		
+	}
+
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+		
+	}
+
+}
