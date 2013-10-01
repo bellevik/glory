309a7243ed749330572449bab7fedb9c7257e95a
Marcus Olsson <marcus@meeloo.se>
2013-10-01 11:14:10 +0200

    Added a public reference to the player object

--- zombie-world/src/se/glory/utilities/WorldHandler.java
+++ zombie-world/src/se/glory/utilities/WorldHandler.java
@@ -1,5 +1,7 @@
 package se.glory.utilities;
 
+import se.glory.entities.Player;
+
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
@@ -10,6 +12,7 @@ public class WorldHandler {
 	public static World world;
 	public static Array<Body> drawableBodies = new Array<Body>();
 	public static Array<Body> removeableBodies = new Array<Body>();
+	public static Player player;
 	
 	public static void createWorld() {
 		world = new World(new Vector2(0, 0), true);
