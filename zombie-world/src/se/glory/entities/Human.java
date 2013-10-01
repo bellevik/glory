fb6765a016551098ffb39af109e8913861fce13f
Marcus Olsson <marcus@meeloo.se>
2013-10-01 11:15:38 +0200

    Added functionality to walk a path, and the fleeing and chasing direction

--- zombie-world/src/se/glory/entities/Human.java
+++ zombie-world/src/se/glory/entities/Human.java
@@ -1,12 +1,46 @@
 package se.glory.entities;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 import se.glory.utilities.Constants;
 import se.glory.utilities.TextureHandler;
 
 public class Human extends MoveableBody implements Creature {
+	private State state = State.IDLE;
+	private ArrayList<Point> walkPath = new ArrayList<Point>();
 	
-	public Human(int x, int y) {
+	public Human(float x, float y) {
 		super(x, y, 15, 15, TextureHandler.humanTexture, Constants.MoveableBodyShape.CIRCLE , Constants.MoveableBodyType.HUMAN);
+	}
+	
+	public void walk() {
+		if (walkPath.size() > 0) {
+			if (Math.abs(walkPath.get(0).x - getBody().getPosition().x) < 0.02 && Math.abs(walkPath.get(0).y - getBody().getPosition().y) < 0.02) {
+				walkPath.remove(0);
+			}
+		}
+		
+		if (walkPath.size() > 0) {
+			float tmpX = walkPath.get(0).x - getBody().getPosition().x;
+			float tmpY = walkPath.get(0).y - getBody().getPosition().y;
+			
+			double size = Math.sqrt(tmpX * tmpX + tmpY * tmpY);
+			tmpX /= size;
+			tmpY /= size;
+			
+			getBody().setLinearVelocity(tmpX, tmpY);
+		} else {
+			setState(State.IDLE);
+		}
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
 	}
 	
 	public enum State {
