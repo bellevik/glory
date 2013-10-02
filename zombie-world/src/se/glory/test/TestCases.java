1ec819847ef532727d7fe455d47eaea8440fb783
VitoInfinito <tomas@hasselquist.se>
2013-10-02 11:21:36 +0200

    Added new test

--- zombie-world/src/se/glory/test/TestCases.java
+++ zombie-world/src/se/glory/test/TestCases.java
@@ -2,9 +2,12 @@ package se.glory.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
 import java.util.ArrayList;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
@@ -12,6 +15,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import se.glory.entities.Creature;
 import se.glory.entities.Human;
 import se.glory.entities.Zombie;
+import se.glory.entities.ai.AStarPathFinder;
 
 public class TestCases {
 
@@ -40,5 +44,50 @@ public class TestCases {
 //		assertsEquals("Human starts at correct y position", expected, actual)
 		
 	//	testHuman.autoUpdateMovement(zombies);
+	}
+	
+	@Test
+	public void testAStarPathFinder() {
+		
+		//Creates the list with blocked points the AStar algorithm has to get through
+		ArrayList<Point> blocked = new ArrayList<Point>();
+		
+		blocked.add(new Point(4, 3));
+		blocked.add(new Point(4, 4));
+		blocked.add(new Point(4, 5));
+		
+		//Creates the correctPath to reference check with the shortestPath
+		ArrayList<Point> correctPath = new ArrayList<Point>();
+		
+		correctPath.add(new Point(3, 5));
+		correctPath.add(new Point(3, 6));
+		correctPath.add(new Point(4, 6));
+		correctPath.add(new Point(5, 6));
+		correctPath.add(new Point(6, 5));
+		correctPath.add(new Point(7, 4));
+		
+		//Creates the false path to doublecheck the assertThat method
+		ArrayList<Point> falsePath = new ArrayList<Point>();
+		
+		falsePath.add(new Point(6, 1));
+		falsePath.add(new Point(4, 4));
+		falsePath.add(new Point(3, 3));
+		falsePath.add(new Point(2, 2));
+		falsePath.add(new Point(1, 1));
+		falsePath.add(new Point(4, 7));
+		
+		int startX = 2;
+		int startY = 4;
+		
+		int goalX = 7;
+		int goalY = 4;
+		
+		//Creates the shortest path through the AStarPathFinder class
+		ArrayList<Point> shortestPath = AStarPathFinder.getShortestPath(startX, startY, goalX, goalY, blocked);
+		
+		//Asserts the correct path has the same points as the shortestPath
+		assertThat(correctPath, is(shortestPath));
+		//Asserts the false path does not have the same points as the shortestPath to make sure the assertThat doesn't accept all paths
+		assertThat(falsePath, not(shortestPath));
 	}
 }
