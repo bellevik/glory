e51e31514874a1f21579d48d3bf71fed11df81b6
Maestro <adam.jilsen@gmail.com>
2013-10-09 14:45:38 +0200

    New splash screen image

--- zombie-world/src/se/glory/zombieworld/screens/MainMenu.java
+++ zombie-world/src/se/glory/zombieworld/screens/MainMenu.java
@@ -5,32 +5,21 @@ import se.glory.zombieworld.utilities.Constants;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MainMenu implements Screen {
 	private Stage stage;
-	private TextureAtlas atlas;
-	private Skin skin;
-	private Image buttonExit, buttonPlay, buttonSettings;
-	private BitmapFont font;
 	
 	private SpriteBatch batch;
+	
 	private Texture backgroundTexture, buttonExitTexture, buttonPlayTexture, buttonSettingsTexture;
-	private boolean pressedPlay = false, pressedSettings = false, pressedExit = false;
-	private int count = 0;
+	private Image buttonExit, buttonPlay, buttonSettings;
     
 	@Override
 	public void render(float delta) {
@@ -43,8 +32,6 @@ public class MainMenu implements Screen {
 		
 		stage.act(delta);
 		stage.draw();
-		
-		drawMenu(batch);
 	}
 
 	@Override
@@ -67,38 +54,33 @@ public class MainMenu implements Screen {
 		
 		//Create buttons
 		buttonPlay = new Image(buttonPlayTexture);
-		buttonPlay.setX(Constants.VIEWPORT_WIDTH/2 - 142/2);
+		buttonPlay.setX(Constants.VIEWPORT_WIDTH/2 - buttonPlayTexture.getWidth()/2);
 		buttonPlay.setY(250);
 		
 		buttonSettings = new Image(buttonSettingsTexture);
-		buttonSettings.setX(Constants.VIEWPORT_WIDTH/2 - 226/2);
-		buttonSettings.setY(175);
+		buttonSettings.setX(Constants.VIEWPORT_WIDTH/2 - buttonSettingsTexture.getWidth()/2);
+		buttonSettings.setY(buttonPlay.getY() - 75);
 		
 		buttonExit = new Image(buttonExitTexture);
-		buttonExit.setX(Constants.VIEWPORT_WIDTH/2 - 144/2);
-		buttonExit.setY(100);
+		buttonExit.setX(Constants.VIEWPORT_WIDTH/2 - buttonExitTexture.getWidth()/2);
+		buttonExit.setY(buttonSettings.getY() - 75);
 
 		//Adding listeners to buttons
 		buttonExit.addListener(new ClickListener(){
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				count = 0;
-				pressedExit = true;
 				Gdx.app.exit();
 			}
 		});
 		buttonSettings.addListener(new ClickListener(){
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				count = 0;
-				pressedSettings = true;
+				
 			}
 		});
 		buttonPlay.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				count = 0;
-				pressedPlay = true;
 				((Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen());
 			}
 		});
@@ -108,40 +90,6 @@ public class MainMenu implements Screen {
 		stage.addActor(buttonExit);
 	}
 
-	public void drawMenu(SpriteBatch batch){
-		if(pressedPlay){
-			buttonPlayTexture = new Texture(Gdx.files.internal("ui/buttonPlayHover.png"));
-			count++;
-			if(count == 50){
-				pressedPlay = false;
-				count = 0;
-			}
-		}else if(pressedSettings){
-			buttonSettingsTexture = new Texture(Gdx.files.internal("ui/buttonSettingsHover.png"));
-			count++;
-			if(count == 50){
-				pressedSettings = false;
-				count = 0;
-			}
-		}else if(pressedExit){
-			buttonExitTexture = new Texture(Gdx.files.internal("ui/buttonExitHover.png"));
-			count++;
-			if(count == 50){
-				pressedExit = false;
-				count = 0;
-			}
-		}else{
-			buttonPlayTexture = new Texture(Gdx.files.internal("ui/buttonPlay.png"));
-			buttonSettingsTexture = new Texture(Gdx.files.internal("ui/buttonSettings.png"));
-			buttonExitTexture = new Texture(Gdx.files.internal("ui/buttonExit.png"));
-		}
-		batch.begin();
-		batch.draw(buttonPlayTexture, Constants.VIEWPORT_WIDTH/2 - 142/2, 250);
-		batch.draw(buttonSettingsTexture, Constants.VIEWPORT_WIDTH/2 - 226/2, 175);
-		batch.draw(buttonExitTexture, Constants.VIEWPORT_WIDTH/2 - 144/2, 100);
-		batch.end();
-	}
-	
 	@Override
 	public void hide() {
 		
