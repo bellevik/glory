6168cbd2cfdd71a88058c684870ef994790a976b
Philip Ekman <ekman1991@gmail.com>
2013-10-10 17:19:01 +0200

    Class to handle score

---
+++ zombie-world/src/se/glory/zombieworld/utilities/Score.java
@@ -0,0 +1,18 @@
+package se.glory.zombieworld.utilities;
+
+import se.glory.zombieworld.utilities.Constants;
+
+public class Score {
+	public static long currentScore;
+	
+	public static void addScore(Constants.ScoreType type) {
+		switch (type) {
+		case KILL_HUMAN: 
+			currentScore -= 111;
+		case KILL_ZOMBIE:
+			currentScore += 97;
+		case TIME_POINT:
+			currentScore += 1;
+		}
+	}
+}
