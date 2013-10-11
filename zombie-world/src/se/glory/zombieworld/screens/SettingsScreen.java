8764a74eb5f8dc35c5154205e168d5236abe317a
Maestro <adam.jilsen@gmail.com>
2013-10-11 14:10:35 +0200

    Added sliders to settingsscreen

--- zombie-world/src/se/glory/zombieworld/screens/SettingsScreen.java
+++ zombie-world/src/se/glory/zombieworld/screens/SettingsScreen.java
@@ -7,28 +7,49 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
 public class SettingsScreen implements Screen {
 	private Stage stage;
 	
 	private SpriteBatch batch;
 	
-	private Texture backgroundTexture, SFXLabelTexture, musicLabelTexture, backButtonTexture;
+	private Texture backgroundTexture, SFXLabelTexture, musicLabelTexture, backButtonTexture, sliderBarTexture, sliderKnobTexture;
 	private Image SFXLabel, musicLabel, backButton;
+	
+	private Slider musicVolume, SFXVolume;
+	
+	private Drawable sliderBar, sliderKnob;
+	
+	private Sprite sliderBarSprite, sliderKnobSprite;
+	
+	private BitmapFont font;
+	
+	private CharSequence SFXValue, musicValue;
     
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(255, 255, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		SFXValue = (int) SFXVolume.getVisualValue() + "%";
+		musicValue = (int) musicVolume.getVisualValue() + "%";
+		
 		batch.begin();
 		batch.draw(backgroundTexture, 0, 0);
+		font.draw(batch, musicValue, musicVolume.getX() + musicVolume.getWidth() + 10, 300);
+		font.draw(batch, SFXValue, SFXVolume.getX() + SFXVolume.getWidth() + 10, 250);
 		batch.end();
+	
 		
 		stage.act(delta);
 		stage.draw();
@@ -41,6 +62,9 @@ public class SettingsScreen implements Screen {
 
 	@Override
 	public void show() {
+		
+		font = new BitmapFont();
+		
 		stage = new Stage();
 		
 		batch = new SpriteBatch();
@@ -50,6 +74,16 @@ public class SettingsScreen implements Screen {
 		SFXLabelTexture = new Texture(Gdx.files.internal("ui/buttonSFX.png"));
 		backButtonTexture = new Texture(Gdx.files.internal("ui/buttonBack.png"));
 		
+		sliderBarTexture = new Texture(Gdx.files.internal("ui/sliderBar.png"));
+		sliderKnobTexture = new Texture(Gdx.files.internal("ui/sliderKnob.png"));
+		
+		sliderBarSprite = new Sprite(sliderBarTexture);
+		sliderKnobSprite = new Sprite(sliderKnobTexture);
+		
+		
+		sliderBar = new SpriteDrawable(sliderBarSprite);
+		sliderKnob = new SpriteDrawable(sliderKnobSprite);
+		
 		Gdx.input.setInputProcessor(stage);
 		
 		//Create buttons
@@ -57,9 +91,20 @@ public class SettingsScreen implements Screen {
 		musicLabel.setX(Constants.VIEWPORT_WIDTH/4 - musicLabelTexture.getWidth()/4);
 		musicLabel.setY(250);
 		
+		musicVolume = new Slider(0, 100, 1, false, new Slider.SliderStyle(sliderBar, sliderKnob));
+		musicVolume.setX(Constants.VIEWPORT_WIDTH/2 - musicVolume.getWidth()/2);
+		musicVolume.setY(250 + musicVolume.getHeight());
+		musicVolume.setValue(100);
+		
+		
 		SFXLabel = new Image(SFXLabelTexture);
 		SFXLabel.setX(Constants.VIEWPORT_WIDTH/4 - SFXLabelTexture.getWidth()/4);
 		SFXLabel.setY(200);
+		
+		SFXVolume = new Slider(0, 100, 1, false, new Slider.SliderStyle(sliderBar, sliderKnob));
+		SFXVolume.setX(Constants.VIEWPORT_WIDTH/2 - SFXVolume.getWidth()/2);
+		SFXVolume.setY(200 + SFXVolume.getHeight());
+		SFXVolume.setValue(100);
 		
 		backButton = new Image(backButtonTexture);
 		backButton.setX(Constants.VIEWPORT_WIDTH/2 - SFXLabelTexture.getWidth()/2);
@@ -85,6 +130,9 @@ public class SettingsScreen implements Screen {
 			}
 		});
 		
+		
+		stage.addActor(SFXVolume);
+		stage.addActor(musicVolume);
 		stage.addActor(musicLabel);
 		stage.addActor(SFXLabel);
 		stage.addActor(backButton);
