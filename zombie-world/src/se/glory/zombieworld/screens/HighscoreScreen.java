feb002132833c7f3568a58e907a9e6f571d91721
Philip Ekman <ekman1991@gmail.com>
2013-10-16 13:56:06 +0200

    Added a highscore screen

---
+++ zombie-world/src/se/glory/zombieworld/screens/HighscoreScreen.java
@@ -0,0 +1,101 @@
+package se.glory.zombieworld.screens;
+
+import se.glory.zombieworld.utilities.Constants;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class HighscoreScreen implements Screen {
+	private Stage stage;
+	private SpriteBatch batch;
+	private Image highscoreLabel, backButton;
+	
+	private Texture backgroundTexture;
+	
+	private BitmapFont font;
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(255, 255, 255, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		
+		batch.begin();
+		batch.draw(backgroundTexture, 0, 0);
+		font.draw(batch, "1.", Constants.VIEWPORT_WIDTH/4, 270);
+		font.draw(batch, "2.", Constants.VIEWPORT_WIDTH/4, 240);
+		font.draw(batch, "3.", Constants.VIEWPORT_WIDTH/4, 210);
+		font.draw(batch, "4.", Constants.VIEWPORT_WIDTH/4, 180);
+		font.draw(batch, "5.", Constants.VIEWPORT_WIDTH/4, 150);
+		batch.end();
+	
+		
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO resize as in game screen
+	}
+
+	@Override
+	public void show() {
+		backgroundTexture = new Texture(Gdx.files.internal("ui/mainMenuBackground.png"));
+		
+		font = new BitmapFont();
+		stage = new Stage();
+		batch = new SpriteBatch();
+		
+		Gdx.input.setInputProcessor(stage);
+		
+		highscoreLabel = new Image(new Texture(Gdx.files.internal("ui/buttonBack.png")));
+		highscoreLabel.setX(Constants.VIEWPORT_WIDTH/2 - highscoreLabel.getWidth()/2);
+		highscoreLabel.setY(295);
+		
+		stage.addActor(highscoreLabel);
+		
+		backButton = new Image(new Texture(Gdx.files.internal("ui/buttonBack.png")));
+		backButton.setX(Constants.VIEWPORT_WIDTH/2 - backButton.getWidth()/2);
+		backButton.setY(20);
+		
+		stage.addActor(backButton);
+		
+		backButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+			}
+		});
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
