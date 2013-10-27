a6af0d6595137de136845cd799bd66496e78641d
bellevik <bellevik@gmail.com>
2013-10-27 13:36:38 +0100

    Added Sikuli-tests

---
+++ documents/SikuliTest/GameTest.sikuli/GameTest.py
@@ -0,0 +1,11 @@
+Settings.DelayBeforeDrop = 3
+click("1382876640211.png")
+wait(1)
+dragDrop("1382876585490.png", "1382876593206.png")
+
+dragDrop("1382876697291.png", "1382876702252.png")
+click("1382876764409.png")
+click("1382876784653.png")
+click("1382876793958.png")
+click("1382876802980.png")
+dragDrop("1382876863205.png", "1382876867380.png")
